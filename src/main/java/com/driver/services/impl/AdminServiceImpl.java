a2306200a75b2d00999a6f063d25abace0830e06
Durga0808 <bejawada.durgaprasad@gmail.com>
2023-08-26 23:51:45 +0530

    dp push

--- src/main/java/com/driver/services/impl/AdminServiceImpl.java
+++ src/main/java/com/driver/services/impl/AdminServiceImpl.java
@@ -49,38 +49,43 @@ public class AdminServiceImpl implements AdminService {
 
     @Override
     public ServiceProvider addCountry(int serviceProviderId, String countryName) throws Exception{
-        if(countryName.equalsIgnoreCase("IND") || countryName.equalsIgnoreCase("USA") || countryName.equalsIgnoreCase("JPN") || countryName.equalsIgnoreCase("CHI") || countryName.equalsIgnoreCase("AUS")){
-            Country country = new Country();
-
-            ServiceProvider serviceProvider = serviceProviderRepository1.findById(serviceProviderId).get();
-
-            if(countryName.equalsIgnoreCase("IND")){
+        StringBuilder sb=new StringBuilder();
+        for(int i=0;i<countryName.length();i++){
+            char c=countryName.charAt(i);
+            if(c>='a'&&c<='z'){
+                c=(char)(c-32);
+            }
+            sb.append(c);
+        }
+        String cname= sb.toString();
+        if(cname.equals("IND")||cname.equals("USA")||cname.equals("JPN")||cname.equals("CHI")||cname.equals("AUS")){
+            Country country=new Country();
+            ServiceProvider serviceProvider=serviceProviderRepository1.findById(serviceProviderId).get();
+            if(cname.equals("IND")){
                 country.setCountryName(CountryName.IND);
                 country.setCode(CountryName.IND.toCode());
             }
-            if(countryName.equalsIgnoreCase("USA")){
+            if(cname.equals("USA")){
                 country.setCountryName(CountryName.USA);
                 country.setCode(CountryName.USA.toCode());
             }
-            if(countryName.equalsIgnoreCase("JPN")){
+            if(cname.equals("JPN")){
                 country.setCountryName(CountryName.JPN);
                 country.setCode(CountryName.JPN.toCode());
             }
-            if(countryName.equalsIgnoreCase("CHI")){
+            if(cname.equals("CHI")){
                 country.setCountryName(CountryName.CHI);
                 country.setCode(CountryName.CHI.toCode());
             }
-            if(countryName.equalsIgnoreCase("AUS")){
+            if(cname.equals("AUS")){
                 country.setCountryName(CountryName.AUS);
                 country.setCode(CountryName.AUS.toCode());
             }
             country.setServiceProvider(serviceProvider);
             serviceProvider.getCountryList().add(country);
             serviceProviderRepository1.save(serviceProvider);
-
             return serviceProvider;
-        }
-        else{
+        }else{
             throw new Exception("Country not found");
         }
     }
